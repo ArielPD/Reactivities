4d78f23670f49dc578071276f43dd184a87eef4d
Ariel Pinto <arielp@goja.com>
2022-02-07 17:16:04 -0400

    End of section 6

--- client-app/src/fatures/activities/details/ActivityDetails.tsx
+++ client-app/src/fatures/activities/details/ActivityDetails.tsx
@@ -5,6 +5,7 @@ interface Props {
     activity: Activity;
     cancelSelectActivity: () => void;
     openForm: (id: string) => void;
+
 }
 
 export default function ActivityDetails({activity, cancelSelectActivity, openForm} : Props){
